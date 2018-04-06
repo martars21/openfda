12977fc913a3d23a4386907365a1a2edd6f48072
martars21 <marta.rose@live.com>
2018-04-06 11:08:54 +0200

    Programa 1

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -1,23 +1,31 @@
-#El programa entra en OpenFDA, se descarga los medicamentos, coge uno y extrae la información pedida.
 #Importo los módulos que voy a utilizar
 import http.client
 import json
 
-headers = {'User-Agent': 'http-client'} #Cabeceras de mi petición. Le digo qué navegador soy.
-conn = http.client.HTTPSConnection("api.fda.gov") #Variable para establecer la conexión con la página web.
-conn.request("GET", "/drug/label.json", None, headers) #Mando una petición a la web de tipo GET.
+headers = {'User-Agent': 'http-client'}
+
+#Establecemos la conexión con la página web
+conn = http.client.HTTPSConnection("api.fda.gov")
+
+conn.request("GET", "/drug/label.json", None, headers)
 #"/drug/label.json" es el recurso, donde me quiero conectar en concreto.
 
+#Respuesta de la página web
+resp1 = conn.getresponse()
 
-resp1 = conn.getresponse() #Esta es la respuesta de la página web.
-print(resp1.status, resp1.reason) #Status es un número, y reason muestra lo que quiere decir ese número. (Para comprobar que funciona bien)
-respuesta = resp1.read().decode("utf-8") #"utf-8" descodifica la respuesta y lee resp1 al completo en formato json.
+#Comprobamos si fuciona bien
+print(resp1.status, resp1.reason)
+if resp1.status == 200:
+    print ("Todo correcto")
+else:
+    print("El recurso no está disponible")
+
+respuesta = resp1.read().decode("utf-8") #Descodificamos la respuesta
 conn.close()
 
-#EL módulo json sirve para tratar algo en formato json como listas y diccionarios.
-#Para "llamar" a los elementos de una lista, pongo entre corchetes la posición donde se encuentra el elemento que quiero imprimir.
-resultado = json.loads(respuesta)
-informacion_medicamento=resultado['results'][0] #Para meterme en el resultado
+resultado = json.loads(respuesta) #Loads convierte al json en un objeto de python (con listas y diccionarios)
+
+informacion_medicamento=resultado['results'][0]
 
 
 #Extraigo el ID, el propósito y el fabricante del medicamento.
--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -2,21 +2,31 @@
 import http.client
 import json
 
-headers = {'User-Agent': 'http-client'} #Cabeceras de mi petición. Le digo que navegador soy.
-conn = http.client.HTTPSConnection("api.fda.gov") #Variable para establecer la conexión con la página web.
-conn.request("GET", "/drug/label.json?limit=10", None, headers) #Mando una petición a la web de tipo GET.
-#"/drug/label.json?limit=10" es el recurso que quiero en concreto.
-#Pongo limit=10 porque quiero que baje 10 objetos. Si no indico nada, bajará por defecto uno (programa anterior).
+headers = {'User-Agent': 'http-client'}
 
-resp1 = conn.getresponse() #Esta es la respuesta de la página web.
-print(resp1.status, resp1.reason) #Status es un número, y reason muestra lo que quiere decir ese número. (Para comprobar que funciona bien)
-respuesta = resp1.read().decode("utf-8") #"utf-8" descodifica la respuesta y lee resp1 al completo en formato json.
+#Establecemos la conexión con la página web
+conn = http.client.HTTPSConnection("api.fda.gov")
+
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
+#Pongo limit=10 porque quiero que baje 10 objetos.
+
+#Respuesta de la página web
+resp1 = conn.getresponse()
+
+#Comprobamos que funciona correctamente
+print(resp1.status, resp1.reason)
+if resp1.status == 200:
+    print ("Todo correcto")
+else:
+    print("El recurso no está disponible")
+
+respuesta = resp1.read().decode("utf-8") #Descodificamos la respuesta
+
 conn.close()
 
-#EL módulo json sirve para tratar algo en formato json como listas y diccionarios.
-resultado = json.loads(respuesta) #Para depurar json
 
-#Ahora el json tiene una lista de 10 medicamentos, entonces la recorro con un bucle for a lo largo de toda su longitud.
+resultado = json.loads(respuesta) #Loads convierte el json en un objeto de python.
+
 for i in range (len (resultado['results'])):
     informacion_medicamento=resultado['results'][i] #El valor de la i irá de 0 y 9 (un número por cada medicamento).
 
